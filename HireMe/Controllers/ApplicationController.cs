7e8ffc29fb4660cb90909176726b220450cb5683
John L <jljacko@outlook.com>
2016-10-14 22:13:07 -0400

    Adding class and Views for Jobs and Applications

---
+++ HireMe/Controllers/ApplicationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// Local namespaces
+using HireMe.Models;
+
+namespace HireMe.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        // GET: Application
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
